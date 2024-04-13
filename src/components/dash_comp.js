a89361f52fb8c80bd46a4eb1c40b4ca32a82f045
RoguePetz <briggsrhema@gmail.com>
2024-04-13 16:54:33 +0100

    made profile description

--- src/components/dash_comp.js
+++ src/components/dash_comp.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import "../style/dash.css"
+import banner from './assets/icons/banner.png'
+import pfp2 from './assets/icons/pfp2.png'
 
 const DashC = () => {
     return(
         <div className='dash-container'>
-            <div>
+            <div className='dash-holder'>
+                <div style={{padding:'30px'}}>
+                    <img src={banner}/>
+                    <img src={pfp2} className='pfp2'/>
+                    <div style={{marginTop:'70px', display:'flex', alignItems:'center',justifyContent:'space-between'}}>
+                        <div>
+                            <h1 >Adam-son Store</h1>
+                            <p className='description'>Another way to improve the readability of a website or app is to use clear and concise navigation. This helps to make it easy for you to find the information or features that you are looking for. It also helps to prevent you from becoming lost or overwhelmed.</p>
+                        </div>
 
-            </div>
+                        <div style={{display:'flex'}}>
+                            <div>Edit storfront</div>
+                            <div>Add product</div>
+                        </div>
+                    </div>
+                </div>
+                </div>
         </div>
     )
 }
