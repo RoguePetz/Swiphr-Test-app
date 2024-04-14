0e3abe50d55002097857e3c0ffd9d4333536ce06
RoguePetz <briggsrhema@gmail.com>
2024-04-14 20:22:59 +0100

    finished

--- src/components/dash_comp.js
+++ src/components/dash_comp.js
@@ -28,133 +28,130 @@ import rise from './assets/icons/stock rise.png'
 const DashC = () => {
     return(
         <div>
-            <div className='dash-container'>
-                <div className='dash-holder'>
-                    <div style={{padding:'30px'}}>
-                        <img className='banner' src={banner}/>
-                        <img src={pfp2} className='pfp2'/>
-                        <div style={{marginTop:'40px', display:'flex', alignItems:'center',justifyContent:'space-between'}}>
-                            <div>
-                                <h1 className='store-name'>Adam-son Store</h1>
-                                <p className='description'>Another way to improve the readability of a website or app is to use clear and concise navigation. This helps to make it easy for you to find the information or features that you are looking for. It also helps to prevent you from becoming lost or overwhelmed.</p>
-                            </div>
-                            <div style={{display:'flex', gap:'10px'}}>
-                                <div className='di2'>Edit storefront</div>
-                                <div className='di'>Add product</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            
-            </div>
-            <div style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
-                <div style={{width:'1300px'}}>
-                    <p style={{fontWeight:'bolder',marginTop:'40px'}}>Overview</p>
-                    <div style={{display:'flex',justifyContent:'space-between',flexWrap:'wrap'}}>
-                        <Card
-                            img ={price1}
-                            img2 ={rise}
-                        />
-                        <Card
-                            img ={price2}
-                            img2 ={rise}
-                        />
-                        <Card
-                            img ={price3}
-                            img2 ={fall}
-                        />
-                    </div>
-                </div>
-            </div>
-            <div  style={{display:'flex',justifyContent:'center', alignItems:'center',flexWrap:'wrap',gap:'30px',marginTop:'50px'}}>
-                <div className='chart'>
-                    <div style={{margin:'20px'}}>
-                        <div style={{display:'flex',justifyContent:'space-between'}}>
-                            <div style={{display:'flex',flexDirection:'column'}}>
-                                <label>Real-Time Sales</label>
-                                <label style={{color:'grey', fontSize:'13px', opacity:'50%'}}>Here are the monthly sales stats</label>
-                            </div>
-                            <img src={order} height={'20px'}/>
-                        </div>
-                        <img src={chart} />
-                    </div>
-
-                </div>
-
-                <div className='chart'>
-                    <div style={{margin:'20px'}}>
-                        <div style={{display:'flex',justifyContent:'space-between'}}>
-                            <div style={{display:'flex',flexDirection:'column'}}>
-                                <label>Top Product</label>
-                                <label style={{color:'grey', fontSize:'13px', opacity:'50%'}}>Here are the most recent financial transaction</label>
-                            </div>
-                            <label>view all</label>
-                        </div>
-                        <div style={{marginTop:'10px',display:'flex',flexWrap:'wrap', justifyContent:'space-between'}}>
-                            <img src={product1} />   
-                            <img src={product2} />   
-                            <img src={product3} />   
-                            <img src={product3} />   
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <div style={{display:'flex',justifyContent:'center'}}>
-                <div className='dash-holder2'>
-                    <div style={{margin:'20px'}}>
-                            <img src={heading} />
-                            <div style={{marginTop:'20px'}}>
-                                <div className='top-tables'>
-                                    <div style={{display:'flex',justifyContent:'space-between',}}>
-                                        <label>Product name</label>
-                                        <div className='twidth' style={{display:'flex',justifyContent:'space-between'}} >
-                                            <label>Date</label>
-                                            <label>Order number</label>
-                                            <label>Price</label>
-                                            <label>status</label>
-                                        </div>
-                                    </div>
+            <div style={{marginLeft:'20px',marginRight:'20px'}}>
+                <div className='dash-container'>
+                    <div className='dash-holder'>
+                        <div style={{padding:'30px'}}>
+                            <img className='banner' src={banner}/>
+                            <img src={pfp2} className='pfp2'/>
+                            <div style={{marginTop:'40px', display:'flex', alignItems:'center',justifyContent:'space-between'}}>
+                                <div>
+                                    <h1 className='store-name'>Adam-son Store</h1>
+                                    <p className='description'>Another way to improve the readability of a website or app is to use clear and concise navigation. This helps to make it easy for you to find the information or features that you are looking for. It also helps to prevent you from becoming lost or overwhelmed.</p>
                                 </div>
-                                <div className='top-table'>
-                                    <div style={{display:'flex',justifyContent:'space-between',margin:'20px'}}>
-                                    <div style={{display:'flex',gap:'10px'}}>
-                                        <img src={pimg} />
-                                        <div style={{display:'flex',flexDirection:'column'}}>
+                                <div style={{display:'flex', gap:'10px'}}>
+                                    <div className='di2'>Edit storefront</div>
+                                    <div className='di'>Add product</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                
+                </div>
+                <div style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
+                    <div style={{width:'1300px'}}>
+                        <p style={{fontWeight:'bolder',marginTop:'40px'}}>Overview</p>
+                        <div style={{display:'flex',justifyContent:'space-between',flexWrap:'wrap'}}>
+                            <Card
+                                img ={price1}
+                                img2 ={rise}
+                            />
+                            <Card
+                                img ={price2}
+                                img2 ={rise}
+                            />
+                            <Card
+                                img ={price3}
+                                img2 ={fall}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div  style={{display:'flex',justifyContent:'center', alignItems:'center',gap:'30px',marginTop:'50px'}}>
+                    <div className='chart'>
+                        <div style={{margin:'20px'}}>
+                            <div style={{display:'flex',justifyContent:'space-between'}}>
+                                <div style={{display:'flex',flexDirection:'column'}}>
+                                    <label>Real-Time Sales</label>
+                                    <label style={{color:'grey', fontSize:'13px', opacity:'50%'}}>Here are the monthly sales stats</label>
+                                </div>
+                                <img src={order} height={'20px'}/>
+                            </div>
+                            <img src={chart} />
+                        </div>
+                    </div>
+                    <div className='chart'>
+                        <div style={{margin:'20px'}}>
+                            <div style={{display:'flex',justifyContent:'space-between'}}>
+                                <div style={{display:'flex',flexDirection:'column'}}>
+                                    <label>Top Product</label>
+                                    <label style={{color:'grey', fontSize:'13px', opacity:'50%'}}>Here are the most recent financial transaction</label>
+                                </div>
+                                <label>view all</label>
+                            </div>
+                            <div style={{marginTop:'10px',display:'flex',flexWrap:'wrap', justifyContent:'space-between'}}>
+                                <img src={product1} />
+                                <img src={product2} />
+                                <img src={product3} />
+                                <img src={product3} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div style={{display:'flex',justifyContent:'center'}}>
+                    <div className='dash-holder2'>
+                        <div style={{margin:'20px'}}>
+                                <img src={heading} />
+                                <div style={{marginTop:'20px'}}>
+                                    <div className='top-tables'>
+                                        <div style={{display:'flex',justifyContent:'space-between',}}>
                                             <label>Product name</label>
-                                            <label>Product description</label>
+                                            <div className='twidth' style={{display:'flex',justifyContent:'space-between'}} >
+                                                <label>Date</label>
+                                                <label>Order number</label>
+                                                <label>Price</label>
+                                                <label>status</label>
+                                            </div>
                                         </div>
                                     </div>
-
-                                        <div className='twidth' style={{display:'flex',justifyContent:'space-between'}} >
-                                            <label>12-April-2023</label>
-                                            <label>#1234567</label>
-                                            <label>N10,000</label>
-                                            <img src={completed} width={'75px'} height={'30px'}/>
+                                    <div className='top-table'>
+                                        <div style={{display:'flex',justifyContent:'space-between',margin:'20px'}}>
+                                        <div style={{display:'flex',gap:'10px'}}>
+                                            <img src={pimg} />
+                                            <div style={{display:'flex',flexDirection:'column'}}>
+                                                <label>Product name</label>
+                                                <label>Product description</label>
+                                            </div>
+                                        </div>
+                                            <div className='twidth' style={{display:'flex',justifyContent:'space-between'}} >
+                                                <label>12-April-2023</label>
+                                                <label>#1234567</label>
+                                                <label>N10,000</label>
+                                                <img src={completed} width={'75px'} height={'30px'}/>
+                                            </div>
                                         </div>
                                     </div>
+                                    <div className='top-table'>
+                                        <div style={{display:'flex',justifyContent:'space-between',margin:'20px'}}>
+                                        <div style={{display:'flex',gap:'10px'}}>
+                                            <img src={pimg} />
+                                            <div style={{display:'flex',flexDirection:'column'}}>
+                                                <label>Product name</label>
+                                                <label>Product description</label>
+                                            </div>
+                                        </div>
+                                            <div className='twidth' style={{display:'flex',justifyContent:'space-between'}} >
+                                                <label>12-April-2023</label>
+                                                <label>#1234567</label>
+                                                <label>N10,000</label>
+                                                <img src={pending} width={'75px'} height={'30px'}/>
+                                            </div>
+                                        </div>
+                                    </div>
+                
                                 </div>
-                                <div className='top-table'>
-                                    <div style={{display:'flex',justifyContent:'space-between',margin:'20px'}}>
-                                    <div style={{display:'flex',gap:'10px'}}>
-                                        <img src={pimg} />
-                                        <div style={{display:'flex',flexDirection:'column'}}>
-                                            <label>Product name</label>
-                                            <label>Product description</label>
-                                        </div>
-                                    </div>
-
-                                        <div className='twidth' style={{display:'flex',justifyContent:'space-between'}} >
-                                            <label>12-April-2023</label>
-                                            <label>#1234567</label>
-                                            <label>N10,000</label>
-                                            <img src={pending} width={'75px'} height={'30px'}/>
-                                        </div>
-                                    </div>
-                                </div>
-                           
-                            </div>
-                        
+                
+                        </div>
                     </div>
                 </div>
             </div>
